fa6929ed3c36eb3eb6d21c323477346719b157a6
Feri Harjulianto <mr.harjulianto@gmail.com>
2024-07-04 03:18:04 +0000

    <feature> dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,36 +1,104 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<!doctype html>
+<html lang="en">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
+    <link rel="icon" href="{{ asset('/assets/images/logo.png') }}" type="image/x-icon" />
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/inter/inter.css') }}" id="main-font-link" />
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/phosphor/duotone/style.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/tabler-icons.min.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/feather.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/fontawesome.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/fonts/material.css') }}" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/style.css') }}" id="main-style-link" />
+    <link rel="stylesheet" href="{{ asset('/assets/css/style-preset.css') }}" />
+</head>
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+<body data-pc-preset="preset-1" data-pc-sidebar-caption="true" data-pc-layout="vertical" data-pc-direction="ltr" data-pc-theme_contrast="" data-pc-theme="light">
+    <div class="page-loader">
+        <div class="bar"></div>
+    </div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+    <nav class="pc-sidebar">
+        <div class="navbar-wrapper">
+            <div class="m-header">
+                <a href="{{ route('dashboard') }}" class="b-brand text-primary">
+                    <img src="{{ asset('/assets/images/logo.png') }}" class="img-fluid logo-lg" alt="logo" />
+                    <span class="badge bg-light-success rounded-pill ms-1 theme-version">v{{ env('APP_VERSION') }}</span>
+                </a>
+            </div>
+            <div class="navbar-content">
+                @include('layouts.menubar')
+            </div>
         </div>
-    </body>
+    </nav>
+
+    <header class="pc-header">
+        @include('layouts.header')
+    </header>
+
+    <div class="pc-container">
+        {{ $slot }}
+    </div>
+
+    <footer class="pc-footer">
+        <div class="footer-wrapper container-fluid">
+            <div class="row">
+                <div class="my-1 col">
+                    <p class="m-0">
+                        &copy; {{ date('Y') }} PT Harjulianto Teknologi Indonesia
+                    </p>
+                </div>
+                <div class="col-auto my-1">
+                    <ul class="mb-0 list-inline footer-link">
+                        <li class="list-inline-item">
+                            v{{ env('APP_VERSION') }}
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+
+    <script src="{{ asset('/assets/js/plugins/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/pages/dashboard-default.js') }}"></script>
+    <script src="{{ asset('/assets/js/plugins/popper.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/plugins/simplebar.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/plugins/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/fonts/custom-font.js') }}"></script>
+    <script src="{{ asset('/assets/js/pcoded.js') }}"></script>
+    <script src="{{ asset('/assets/js/plugins/feather.min.js') }}"></script>
+
+    <script>
+        layout_change('light');
+    </script>
+
+    <script>
+        change_box_container('false');
+    </script>
+
+    <script>
+        layout_caption_change('true');
+    </script>
+
+    <script>
+        layout_rtl_change('false');
+    </script>
+
+    <script>
+        preset_change('preset-1');
+    </script>
+
+    <script>
+        main_layout_change('vertical');
+    </script>
+
+
+</body>
+
 </html>
---
+++ resources/views/layouts/menubar.blade.php
@@ -0,0 +1,160 @@
+<div class="card pc-user-card">
+    <div class="card-body">
+        <div class="d-flex align-items-center">
+            <div class="flex-shrink-0">
+                <img src="https://eu.ui-avatars.com/api/?background=0D8ABC&color=fff&name={{ Auth::user()->name }}" alt="user-image" class="user-avtar wid-45 rounded-circle" />
+            </div>
+            <div class="flex-grow-1 ms-3 me-2">
+                <h6 class="mb-0">{{ Auth::user()->name }}</h6>
+                <small>Administrator</small>
+            </div>
+            <a class="btn btn-icon btn-link-secondary avtar" data-bs-toggle="collapse" href="#pc_sidebar_userlink">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-sort-outline"></use>
+                </svg>
+            </a>
+        </div>
+        <div class="collapse pc-user-links" id="pc_sidebar_userlink">
+            <div class="pt-3">
+                <a href="{{ route('profile.edit') }}">
+                    <i class="ti ti-user"></i>
+                    <span>My Account</span>
+                </a>
+                <a href="#!" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                    <i class="ti ti-power"></i>
+                    <span>Logout</span>
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<ul class="pc-navbar">
+    <li class="pc-item pc-caption">
+        <label>FM Team Management</label>
+    </li>
+    <li class="pc-item">
+        <a href="{{ route('dashboard') }}" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-status-up"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Dashboard</span>
+        </a>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-user-add"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Transfer</span>
+        </a>
+    </li>
+
+    <li class="pc-item pc-caption">
+        <label>League</label>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-layer"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Classement</span>
+        </a>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-presentation-chart"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Statistics</span>
+        </a>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-video-play"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Match Result</span>
+        </a>
+    </li>
+
+    <li class="pc-item pc-caption">
+        <label>Cup</label>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-layer"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Classement</span>
+        </a>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-presentation-chart"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Statistics</span>
+        </a>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-video-play"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Match Result</span>
+        </a>
+    </li>
+
+    <li class="pc-item pc-caption">
+        <label>My Team</label>
+    </li>
+    <li class="pc-item">
+        <a href="#" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-presentation-chart"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Player Statistics</span>
+        </a>
+    </li>
+
+    <li class="pc-item pc-caption">
+        <label>Admin Panel</label>
+    </li>
+
+    <li class="pc-item pc-hasmenu">
+        <a href="#!" class="pc-link">
+            <span class="pc-micon">
+                <svg class="pc-icon">
+                    <use xlink:href="#custom-cpu-charge"></use>
+                </svg>
+            </span>
+            <span class="pc-mtext">Configuration</span><span class="pc-arrow"><i data-feather="chevron-right"></i></span>
+        </a>
+        <ul class="pc-submenu">
+            <li class="pc-item"><a class="pc-link" href="#">League & Cup</a></li>
+            <li class="pc-item"><a class="pc-link" href="#">Teams Database</a></li>
+        </ul>
+    </li>
+</ul>
